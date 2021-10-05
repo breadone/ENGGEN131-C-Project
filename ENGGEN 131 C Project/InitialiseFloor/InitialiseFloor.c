a8377a6278baba460da49706809a6fd97f8a6cb2
pradyun <pradyun.setti@gmail.com>
2021-10-05 15:43:38 +1300

    InitaliseFloor done and pretty much commented?

---
+++ ENGGEN 131 C Project/InitialiseFloor/InitialiseFloor.c
@@ -0,0 +1,56 @@
+//
+//  InitialiseFloor.c
+//  ENGGEN 131 C Project
+//
+//  Created by Pradyun Setti on 5/10/21.
+//
+
+#include "InitialiseFloor.h"
+/*
+InitialiseFloor takes a 2D array and initialises it to fit with the rules of the game, with boundary, vacant, entry, and exit points
+Input:
+    - floor: the 2d array to be modified
+    - entryboundary: either T, B, L, R, to decide which wall the entry point will be
+    - index: how far along the chosen wall the entry will be
+ */
+void InitialiseFloor(int floor[NUM_ROWS][NUM_COLS], char entryboundary, int index) {
+    // STEP ONE: initialise boundaries
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = 0; j < NUM_COLS; j++) {
+            // condition one: if the current element is on the bottom or top row of the array
+            if (i == 0 || i == NUM_ROWS-1) {
+                floor[i][j] = BOUNDARY;
+                continue;
+            }
+            
+            // else: if element is at the left or rightmost side, then boundary, else vacant
+            if (j == 0 || j == NUM_COLS-1) {
+                floor[i][j] = BOUNDARY;
+            } else {
+                floor[i][j] = VACANT;
+            }
+        }
+    }
+    
+    // STEP TWO: add entry and exit
+    switch (entryboundary) {
+        case 'T':
+            floor[0][index] = ENTRY;
+            floor[NUM_ROWS-1][index] = EXIT;
+            break;
+        case 'B':
+            floor[NUM_ROWS-1][index] = ENTRY;
+            floor[0][index] = EXIT;
+            break;
+        case 'L':
+            floor[index][0] = ENTRY;
+            floor[index][NUM_COLS-1] = EXIT;
+            break;
+        case 'R':
+            floor[index][NUM_COLS-1] = ENTRY;
+            floor[index][0] = EXIT;
+            break;
+        default:
+            break;
+    }
+}
--- ENGGEN 131 C Project/main.c
+++ ENGGEN 131 C Project/main.c
@@ -4,11 +4,20 @@
 //
 //  Created by Pradyun Setti on 5/10/21.
 //
-
+#include "InitialiseFloor.h"
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    int floor[NUM_ROWS][NUM_COLS];
+    
+    InitialiseFloor(floor, 'T', 2);
+    
+    for (int i = 0; i < NUM_ROWS; i++) {
+        for (int j = 0; j < NUM_COLS; j++) {
+            printf("%d ", floor[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
