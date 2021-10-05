5fdfddac0da012375565629b9e6de0cc68319fa2
pradyun <pradyun.setti@gmail.com>
2021-10-05 14:41:12 +1300

    Initial Commit

---
+++ ENGGEN 131 C Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ENGGEN 131 C Project
+//
+//  Created by Pradyun Setti on 5/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
